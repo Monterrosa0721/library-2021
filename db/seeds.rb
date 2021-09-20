6c54372af276e7d84c866b1698aa16bd40d01421
Benjamin Monterrosa <benjamindelgado1994@gmail.com>
2021-09-19 20:06:14 -0600

    seed admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-admin.create(name: "admin", email: "admin@test.com", password: "123456")
+Admin.create(name: "admin", email: "admin@test.com", password: "123456")
 
